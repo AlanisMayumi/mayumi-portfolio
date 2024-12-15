a6f4b45db184d35a901b726de94b6e8f5ae73ba5
AlanisMayumi <mayumi.alanis@gmail.com>
2024-12-15 18:29:44 -0300

    profile picture

--- src/Profile/index.tsx
+++ src/Profile/index.tsx
@@ -4,7 +4,14 @@ import { profileData } from "./utils";
 const Profile = () => {
   return (
     <Box>
-      <Avatar alt={profileData.name} />
+      <Avatar
+        alt={profileData.name}
+        src={profileData.picture}
+        sx={{
+          width: 200,
+          height: 200,
+        }}
+      />
     </Box>
   );
 };
---
+++ src/Profile/utils.ts
@@ -0,0 +1,5 @@
+export const profileData = {
+  name: "Alanis Mayumi",
+  picture:
+    "https://media.licdn.com/dms/image/v2/D4D03AQEvj595C-mqcA/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1727352017932?e=1740009600&v=beta&t=k_dpy2YsI2tV0kdb0XC4Lqf_g6eXezS-SgPYV2bSjHk",
+};
